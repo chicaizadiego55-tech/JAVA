eb580efcef1223f16c677cfea0d34abc4dda30ee
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-08-04 19:19:46 -0500

    Agregar constructores sobrecargados a Cuenta

--- JAVA/Taller1/src/clearminds/cuentas/Cuenta.java
+++ JAVA/Taller1/src/clearminds/cuentas/Cuenta.java
@@ -8,7 +8,21 @@ public class Cuenta {
     private String tipo;
     private double saldo;
     
+    // Constructor que recibe solo el ID
+    public Cuenta(String id) {
+        this.id = id;
+        this.tipo = "A";
+        this.saldo = 0;
+    }
+
+    // Constructor completo
+    public Cuenta(String id, String tipo, double saldo) {
+        this.id = id;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
     
+}
     
 
    
