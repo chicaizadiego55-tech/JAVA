98eadbdca7d7d9e3d799cb90eeba0a275f03502a
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-08-04 19:21:20 -0500

    Implementar métodos imprimir e imprimirConMiEstilo

--- JAVA/Taller1/src/clearminds/cuentas/Cuenta.java
+++ JAVA/Taller1/src/clearminds/cuentas/Cuenta.java
@@ -22,6 +22,54 @@ public class Cuenta {
         this.saldo = saldo;
     }
     
+    // Solo Getter para id
+    public String getId() {
+        return id;
+    }
+
+    // Getter y Setter para tipo
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    // Getter y Setter para saldo
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    // Método imprimir
+    public void imprimir() {
+
+        System.out.println("************************");
+        System.out.println("CUENTA");
+        System.out.println("************************");
+        System.out.println("Número de Cuenta: " + id);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Saldo: USD " + saldo);
+        System.out.println("************************");
+    }
+
+    // Método personalizado
+    public void imprimirConMiEstilo() {
+
+        System.out.println("===============================");
+        System.out.println("      INFORMACIÓN CUENTA");
+        System.out.println("===============================");
+        System.out.println("ID      : " + id);
+        System.out.println("TIPO    : " + tipo);
+        System.out.println("SALDO   : USD " + saldo);
+        System.out.println("===============================");
+    }
+
+    
 }
     
 
